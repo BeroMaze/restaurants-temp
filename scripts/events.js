69cef51551dca051e8960a013bbad30c02c3ca0f
BeroMaze <Berning.corey@gmail.com>
2016-09-09 12:47:34 -0700

    add link highlighting

--- scripts/events.js
+++ scripts/events.js
@@ -7,18 +7,26 @@ $('#navList li').on('click', function(event) {
   if ($(this).text() === 'Menu') {
     $('.mainPages').hide();
     $('#menuPage').show();
+    $('#navList li').removeClass('mainMenuOn');
+    $(this).addClass('mainMenuOn');
   }
   else if ($(this).text() === 'Events') {
     $('.mainPages').hide();
     $('#eventsPage').show();
+    $('#navList li').removeClass('mainMenuOn');
+    $(this).addClass('mainMenuOn');
   }
   else if ($(this).text() === 'About') {
     $('.mainPages').hide();
     $('#aboutPage').show();
+    $('#navList li').removeClass('mainMenuOn');
+    $(this).addClass('mainMenuOn');
   }
   else if ($(this).text() === 'ECT') {
     $('.mainPages').hide();
     $('#ectPage').show();
+    $('#navList li').removeClass('mainMenuOn');
+    $(this).addClass('mainMenuOn');
   }
 });
 
@@ -27,17 +35,25 @@ $('#menuOptions li').on('click', function(event) {
   if ($(this).text() === 'Breakfast') {
     $('.menuPic').hide();
     $('#breakfastPic').show();
+    $('#menuOptions li').removeClass('menuLinkOn');
+    $(this).addClass('menuLinkOn');
   }
   else if ($(this).text() === 'Lunch') {
     $('.menuPic').hide();
     $('#lunchPic').show();
+    $('#menuOptions li').removeClass('menuLinkOn');
+    $(this).addClass('menuLinkOn');
   }
   else if ($(this).text() === 'Dinner') {
     $('.menuPic').hide();
     $('#dinnerPic').show();
+    $('#menuOptions li').removeClass('menuLinkOn');
+    $(this).addClass('menuLinkOn');
   }
   else if ($(this).text() === 'Happy Hour') {
     $('.menuPic').hide();
     $('#happyhourPic').show();
+    $('#menuOptions li').removeClass('menuLinkOn');
+    $(this).addClass('menuLinkOn');
   }
 });
